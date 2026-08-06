d9e317bca139b9cb7b51f7c31bad87acc442f00c
GuilhermeAbreu267 <155022402+GuilhermeAbreu267@users.noreply.github.com>
2026-08-05 22:27:02 -0300

    Add files via upload

---
+++ aula 2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+	int a,b,c,
+    float resultado,
+	
+	a =10,
+	b =10;
+    c =10;
+	
+	print ("a+b+c")/3;
+	print("resultado %f,resultado");
+
+	return 0;
+}
