7ea70c1c1bfde56dab9e589ebd7ccc757680aa8d
GuilhermeAbreu267 <155022402+GuilhermeAbreu267@users.noreply.github.com>
2026-08-10 22:30:46 -0300

    Add files via upload

---
+++ aula 3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define pi 3.14159
+
+int main(int argc, char *argv[]) {
+	//area do circulo
+	float raio, area, RaioQuadrado;
+	
+	printf("Insiran o valor R: \n");
+	scanf("%f", &raio);
+	
+	RaioQuadrado = raio*raio;
+	area = pi * RaioQuadrado;
+	
+	printf("A area do circulo de raio %f = f%",raio,area);
+	
+	//area do trapesio
+	
+	float raio, area, RaioTrapesio;
+	
+	print("Area de trapezio %f = f%", area,raiotrapezio:")
+	
+
+		return 0;
+}
